f26dde0a6ec8fdd1f60f5217081399390c236374
Daniel Lando <daniel.sorridi@gmail.com>
2021-03-19 14:37:34 +0100

    wip dropped cubejs, working on a custom backend

---
+++ backend/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const db = require('../db')
+
+const router = express.Router()
+
+db.init()
+
+/* GET home page. */
+router.post('/metrics', async (req, res) => {
+  try {
+    const { table, data } = req.body
+    const result = await db.upsert(table, data)
+    res.json({ success: true, result })
+  } catch (error) {
+    res.json({ success: false, error })
+  }
+})
+
+module.exports = router
