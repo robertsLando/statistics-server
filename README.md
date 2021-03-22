2525badb3ac970e596b4db083b2ff763bea5777f
Daniel Lando <daniel.sorridi@gmail.com>
2021-03-22 17:57:36 +0100

    add update db api

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Full stack that provides all the tools to store and visualize custom metrics usi
 - mongodb 
 - node-red: http://localhost:1880
 - Backend (for apis): http://localhost:5000
-- Mongodb charts: http://localhost
+- Mongodb charts: http://localhost:8080
+
 ## Setup
 
 ```bash
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const db = require('../db')
+const got = require('got')
 
 const router = express.Router()
 
@@ -8,6 +9,25 @@ router.post('/metrics', async (req, res) => {
   try {
     const { collection, data } = req.body
     const result = await db.upsert(collection, data)
+    res.json({ success: true, result })
+  } catch (error) {
+    res.json({ success: false, error })
+  }
+})
+
+router.post('/update-db', async (req, res) => {
+  try {
+    const response = await got('https://raw.githubusercontent.com/zwave-js/node-zwave-js/master/packages/config/config/manufacturers.json').json()
+
+    const manufacturers = Object.keys(response).map(h => {
+      return {
+        hex: h,
+        name: response[h]
+      }
+    })
+
+    const result = await db.upsert('manufacturer', manufacturers)
+
     res.json({ success: true, result })
   } catch (error) {
     res.json({ success: false, error })
