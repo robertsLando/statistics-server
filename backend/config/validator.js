9f0684b54e0d4128a756d06bfccf85710aef1519
Dominic Griesel <d.griesel@gmx.net>
2021-04-01 17:49:56 +0200

    feat: add basic validation and some types

--- backend/config/validator.js
+++ backend/config/validator.js
@@ -6,16 +6,44 @@ const validator = require('express-joi-validation').createValidator({})
 // for each api define the validators schemas
 // available validators here are for `body` `query` `params` `headers` `respone` `fields`
 // more info: https://www.npmjs.com/package/express-joi-validation#api
+
+// schemas is a dictionary of the form
+// {
+//   route1: {
+//     body: { // or one of the others
+//       // Joi schema
+//     }
+//   },
+//   // ... more routes
+// }
+/** @type {Record<string, Partial<Record<import("express-joi-validation").ContainerTypes, Joi.ObjectSchema>>>} */
 const schemas = {
   [APIs.metrics]: {
     body: Joi.object({
-      collection: Joi.string(),
+      collection: Joi.string().optional(),
+      // data must be an array (between 1 and 10 items) of objects that...
       data: Joi.array()
+        .required()
+        .min(1)
+        .max(10)
+        .items(
+          Joi.object({
+            // ...have at least an "id" property
+            id: Joi.string().required().min(1).max(100),
+            // ... have no date property, because that is used internally
+            date: Joi.forbidden()
+            // define additional allowed properties here, or use `{ allowUnknown: true }` in the validation call
+          })
+        )
     })
   }
 }
 
+/**
+ * @param {string} apiName
+ */
 module.exports = function (apiName) {
+  /** @type {import("express").RequestHandler[]} */
   const validators = []
   for (const v in schemas[apiName]) {
     validators.push(validator[v](schemas[apiName][v]))
